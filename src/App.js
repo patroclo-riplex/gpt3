0370a1b5d24a12e6327fb41d36fb9968c27f8f2b
patroclo-riplex <gofo_02@hotmail.com>
2023-01-14 20:50:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Cta, Brand, Navbar } from './components';
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient_bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
